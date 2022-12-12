eb1ba5ac839f9a2ad2f7741c1fc85e7523ce66f4
lergvot <egorovsergey93@live.com>
2022-12-12 19:35:27 +0300

    Update test_3_4_step_7_fixture.py

--- test_3_4_step_7_fixture.py
+++ test_3_4_step_7_fixture.py
@@ -1,30 +1,26 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-link = "http://selenium1py.pythonanywhere.com/"
 
 
-@pytest.fixture
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
+
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
 
 @pytest.fixture(autouse=True)
-def prepare_data():
-    print()
-    print("preparing some critical data for every test")
+def print_smiling_faces():
+    print(":-Р", "\n")
 
 
-class TestMainPage1():
-    def test_guest_should_see_login_link(self, browser):
-        # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
-        browser.get(link)
-        browser.find_element(By.CSS_SELECTOR, "#login_link")
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        # какие-то проверки
 
-    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        browser.get(link)
-        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+    def test_second_smiling_faces(self, prepare_faces):
+        # какие-то проверки
